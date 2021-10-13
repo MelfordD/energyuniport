717cf477b3f4411ff85845231e17674c63c0a98d
MelfordD <melford.deekay@gmail.com>
2021-10-13 00:15:07 -0700

    whitenoise and secure ssl settings

--- energyuniport/settings.py
+++ energyuniport/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'energyuniport.urls'
@@ -115,6 +116,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+SECURE_SSL_REDIRECT = True
 
 
 # Static files (CSS, JavaScript, Images)
