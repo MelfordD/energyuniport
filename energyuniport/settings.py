1d390fab3327e0f18d3570fa63c32759077da9ee
MelfordD <melford.deekay@gmail.com>
2021-11-21 19:36:01 -0800

    disabled static4

--- energyuniport/settings.py
+++ energyuniport/settings.py
@@ -155,6 +155,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_PATH, 'static'),
 )
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
